4d66313549ac6c0e1242a62faee2c444b182edd4
Elvy1999 <elvydecoo99@gmail.com>
2024-10-14 22:36:16 -0400

    added two numbers in c

--- c_Learning/working.c
+++ c_Learning/working.c
@@ -2,6 +2,13 @@
 
 int main(){
   printf("\nHello, World\n");
+  int num1,num2;
+  printf("Enter the 1st number");
+  scanf("%d", &num1);
+  printf("Enter the 2nd number");
+  scanf("%d", &num2);
+  int num3 = num1 + num2;
+  printf("The sum of the two numbers is %d\n",num3);
   return 0;
 }
 
