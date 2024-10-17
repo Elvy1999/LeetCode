be490f6234e71c9d8d2f757fd15d2455091b728a
Elvy1999 <elvydecoo99@gmail.com>
2024-10-17 12:14:23 -0400

    created a menu for the program

--- c_Learning/assignments-main/assignments-main/A01/password.c
+++ c_Learning/assignments-main/assignments-main/A01/password.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
 int main() {
+  char str[100];
+  printf("Enter a password \n");
+  scanf("%99s", str);
+  int i;
+  int length = strlen(str);
+  for(i = 0; i < length; i ++ ){
+    if(str[i] == 'e') str[i] = '3';
+    else if (str[i] == 'l') str[i] = '1';
+    else if (str[i] == 'a') str[i] = '@';
+  }
+  printf("The modified password: %s\n", str);
   return 0;
 }
--- c_Learning/assignments-main/assignments-main/A01/rpc.c
+++ c_Learning/assignments-main/assignments-main/A01/rpc.c
@@ -1,8 +1,36 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
+
+// Define the `snack` struct
+struct snack {
+  char name[100];
+  float price;
+  int quantity;
+};
+// Declare the `menu` function with the correct return type and parameters
+void menu(struct snack snack1, struct snack snack2, struct snack snack3);
+
+// Main function
 int main() {
-  srand(time(0));
+  srand(time(0)); // Seed the random number generator
+
+  // Initialize the snacks
+  struct snack s1 = {"Coco Puffs", 1.50, 4};
+  struct snack s2 = {"Manchego cheese", 15.50, 6};
+  struct snack s3 = {"Magic beans", 0.50, 0};
+
+  // Call the menu function
+  menu(s1, s2, s3);
+
   return 0;
 }
 
+// Define the `menu` function
+void menu(struct snack snack1, struct snack snack2, struct snack snack3) {
+   printf("Available Snacks:\n");
+   printf("0) %s\t\tcost: $%.2f\tquantity: %d\n", snack1.name, snack1.price, snack1.quantity);
+   printf("1) %s\tcost: $%.2f\tquantity: %d\n", snack2.name, snack2.price, snack2.quantity);
+   printf("2) %s\t\tcost: $%.2f\tquantity: %d\n", snack3.name, snack3.price, snack3.quantity);
+}
