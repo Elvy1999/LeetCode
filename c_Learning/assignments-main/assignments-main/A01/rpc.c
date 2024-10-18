80c28bc346c8cb7278ae63a5f3e5d03f158ffc20
Elvy1999 <elvydecoo99@gmail.com>
2024-10-17 20:08:04 -0400

    Finished the program

--- c_Learning/assignments-main/assignments-main/A01/rpc.c
+++ c_Learning/assignments-main/assignments-main/A01/rpc.c
@@ -21,8 +21,59 @@ int main() {
   struct snack s2 = {"Manchego cheese", 15.50, 6};
   struct snack s3 = {"Magic beans", 0.50, 0};
 
+  int choice;
+  float money;
+  printf("Welcome to the snack shop!\n");
+  printf("How much money do you have?$");
+  scanf("%f", &money);
   // Call the menu function
   menu(s1, s2, s3);
+  printf("\n what snack would you like?\n");
+  scanf("%d", &choice);
+  if(choice == 0){
+    if(money >= s1.price && s1.quantity > 0){
+      money -= s1.price;
+      s1.quantity--;
+      printf("You bought a %s for $%.2f\n", s1.name, s1.price);
+      printf("you have $%.2f left\n", money);
+    }
+    else if(money < s1.price){
+      printf("Sorry you cant afford it\n");
+    }
+    else{
+      printf("Sorry we are out of %s\n", s1.name);
+    }
+  }
+    if(choice == 1){
+      if(money >= s2.price && s2.quantity > 0){
+        money -= s2.price;
+        s2.quantity--;
+        printf("You bought a %s for $%.2f\n", s2.name, s2.price);
+        printf("you have $%.2f left\n", money);
+      }
+      else if(money < s2.price){
+        printf("Sorry you cant afford it\n");
+      }
+      else{
+        printf("Sorry we are out of %s\n", s2.name);
+      }
+    }
+      if(choice == 2){
+        if(money >= s3.price && s3.quantity > 0){
+          money -= s3.price;
+          s3.quantity--;
+          printf("You bought a %s for $%.2f\n", s3.name, s3.price);
+          printf("you have $%.2f left\n", money);
+        }
+        else if(money < s3.price){
+          printf("Sorry you cant afford it\n");
+        }
+        else{
+          printf("Sorry we are out of %s\n", s3.name);
+        }
+      }
+  
+
 
   return 0;
 }
@@ -34,3 +85,5 @@ void menu(struct snack snack1, struct snack snack2, struct snack snack3) {
    printf("1) %s\tcost: $%.2f\tquantity: %d\n", snack2.name, snack2.price, snack2.quantity);
    printf("2) %s\t\tcost: $%.2f\tquantity: %d\n", snack3.name, snack3.price, snack3.quantity);
 }
+
+
