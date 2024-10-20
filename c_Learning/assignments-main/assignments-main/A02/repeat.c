71761b5eb64d7f5fdb97fd6cfa3fe56022469756
Elvy1999 <elvydecoo99@gmail.com>
2024-10-20 12:51:01 -0400

    Finished the repeat program

--- c_Learning/assignments-main/assignments-main/A02/repeat.c
+++ c_Learning/assignments-main/assignments-main/A02/repeat.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main() {
+int main()
+{
+  char str[32];
+  int number;
+  printf("Enter a word: ");
+  scanf("%31s", str);
+  printf("Enter a count: ");
+  scanf("%d", &number);
+  char *ptr = malloc(sizeof(char) * (strlen(str) + 1));
+  if (ptr == NULL)
+  {
+    printf("Memory allocation failed!\n");
+    exit(1);
+  }
+  strcpy(ptr, str);
+  for (int i = 0; i < number; i++)
+  {
+    printf("%s", ptr);
+  }
+  printf("\n");
+  free(ptr);
   return 0;
 }
