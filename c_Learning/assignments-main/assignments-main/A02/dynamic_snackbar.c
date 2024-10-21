5c0049b0e8d007c376ad4b69f00c1b97f0152528
Elvy1999 <elvydecoo99@gmail.com>
2024-10-21 10:26:48 -0400

    Finisdhed dynamic snack bar utilizing dynamic memory allocation

--- c_Learning/assignments-main/assignments-main/A02/dynamic_snackbar.c
+++ c_Learning/assignments-main/assignments-main/A02/dynamic_snackbar.c
@@ -2,12 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct snack {
+struct snack
+{
   char name[32];
   float cost;
   int quantity;
 };
 
-int main() {
+int main()
+{
+  int snackNum;
+  printf("Enter a number of snack:");
+  scanf("%d", &snackNum);
+  struct snack *snacks = malloc(sizeof(struct snack) * snackNum);
+  for (int i = 0; i < snackNum; i++)
+  {
+    printf("Enter snack name:");
+    scanf("%32s", snacks[i].name);
+    printf("Enter snack cost:");
+    scanf("%f", &snacks[i].cost);
+    printf("Enter snack quantity:");
+    scanf("%d", &snacks[i].quantity);
+  }
+  for (int i = 0; i < snackNum; i++)
+  {
+    printf("%s\t\tcost: $%.2f\tquantity: %d\n",
+           snacks[i].name, snacks[i].cost, snacks[i].quantity);
+  }
   return 0;
 }
