c80e042b31d9dae400a64ec94e3e2e3e8df6048c
Elvy1999 <elvydecoo99@gmail.com>
2024-10-25 04:40:57 -0400

    implemented the palindrome checker function with the help of chatgpt

--- c_Learning/assignments-main/assignments-main/A02/calculator.c
+++ c_Learning/assignments-main/assignments-main/A02/calculator.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <ctype.h>
 
-
 int main()
 {
   int num1, num2;
@@ -12,25 +11,29 @@ int main()
   scanf("%d %d", &num1, &num2);
   printf("Enter the operation you would like to perform (+,-,*,/): ");
   scanf(" %c", &op);
-  switch (op) {
-    case '+':
-      printf("The result is: %d\n", num1 + num2);
-      break;
-    case '-':
-      printf("The result is: %d\n", num1 - num2);
-      break;
-    case '*':
-      printf("The result is: %d\n", num1 * num2);
-      break;
-    case '/':
-      if (num2 == 0) {
-        printf("Cannot divide by zero\n");
-      } else {
-        printf("The result is: %d\n", num1 / num2);
-      }
-      break;
-    default:
-      printf("Invalid operator\n");
+  switch (op)
+  {
+  case '+':
+    printf("The result is: %d\n", num1 + num2);
+    break;
+  case '-':
+    printf("The result is: %d\n", num1 - num2);
+    break;
+  case '*':
+    printf("The result is: %d\n", num1 * num2);
+    break;
+  case '/':
+    if (num2 == 0)
+    {
+      printf("Cannot divide by zero\n");
+    }
+    else
+    {
+      printf("The result is: %d\n", num1 / num2);
+    }
+    break;
+  default:
+    printf("Invalid operator\n");
   }
   return 0;
-  }
+}
--- c_Learning/working2.c
+++ c_Learning/working2.c
@@ -1,31 +1,96 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 
-// Take an array of integers as input and find its sum and average.
+int isPalindrome(char str[])
+{
+  int start = 0;
+  int end = strlen(str) - 1;
+
+  while (start < end)
+  {
+    // Skip non-alphanumeric characters
+    if (!isalnum(str[start]))
+    {
+      start++;
+    }
+    else if (!isalnum(str[end]))
+    {
+      end--;
+    }
+    else
+    {
+      // Compare characters (ignoring case)
+      if (tolower(str[start]) != tolower(str[end]))
+      {
+        return 0; // Not a palindrome
+      }
+      start++;
+      end--;
+    }
+  }
+  return 1; // It's a palindrome
+}
+
+int isPalindrome2(char str[])
+{
+  int start = 0;
+  int end = strlen(str) - 1;
+  while (start < end)
+  {
+    if (!isalnum(str[start]))
+      start++;
+    else if (!isalnum(str[end]))
+      end--;
+    else
+    {
+      if (tolower(str[start]) != tolower(str[end]))
+        return 0;
+    }
+    start++;
+    end--;
+  }
+  return 1;
+}
+
 int main()
 {
-  printf("\nEnter a list of number to calculate the sum and the average for them\n");
-  printf("How many numbers are going to be in your list:");
-  int numbers;
-  scanf("%d", &numbers);
-  int *arr = malloc(sizeof(int) * numbers);
-  int sum = 0, number;
-  float average;
-  for (int i = 0; i < numbers; i++)
-  {
-    printf("\nEnter a number:");
-    scanf("%d", &number);
-    sum += number;
-    arr[i] = number;
-  }
-  average = sum / numbers;
-  printf("The numbers you entered were:");
-  for (int i = 0; i < numbers; i++)
-  {
-    printf(" %d", arr[i]);
-  }
-  printf("\n");
-  printf("The sum is %d, and the average is %.2f\n", sum, average);
+  // Take an array of integers as input and find its sum and average.
+  // printf("\nEnter a list of number to calculate the sum and the average for them\n");
+  // printf("How many numbers are going to be in your list:");
+  // int numbers;
+  // scanf("%d", &numbers);
+  // int *arr = malloc(sizeof(int) * numbers);
+  // int sum = 0, number;
+  // float average;
+  // for (int i = 0; i < numbers; i++)
+  // {
+  //   printf("\nEnter a number:");
+  //   scanf("%d", &number);
+  //   sum += number;
+  //   arr[i] = number;
+  // }
+  // average = sum / numbers;
+  // printf("The numbers you entered were:");
+  // for (int i = 0; i < numbers; i++)
+  // {
+  //   printf(" %d", arr[i]);
+  // }
+  // printf("\n");
+  // printf("The sum is %d, and the average is %.2f\n", sum, average);
+  //
+
+  // check if a string is a palindrome
+
+  printf("Enter a string to see if it is a palindrome:");
+  char string[100];
+  scanf("%99s", string);
+  printf("The word you entered was %s\n", string);
+  int result = isPalindrome2(string) == 1 ? 1 : 0;
+  if (result == 1)
+    printf("%s is a palindrome\n", string);
+  else
+    printf("%s is not a palindrome\n", string);
   return 0;
 }
