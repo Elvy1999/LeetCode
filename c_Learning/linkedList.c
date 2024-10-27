81a764a7e63023f4beb86f094aa5b85f07373bc1
Elvy1999 <elvydecoo99@gmail.com>
2024-10-26 23:28:44 -0400

    Finished the deleteNode function

---
+++ c_Learning/linkedList.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+struct Node
+{
+  struct Node *next;
+  int data;
+};
+struct Node *createNewNode(int data)
+{
+  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+  newNode->data = data;
+  newNode->next = NULL;
+  return newNode;
+}
+
+void insertAtBeginning(struct Node **head, int value)
+{
+  struct Node *newNode = createNewNode(value);
+  newNode->next = head;
+  *head = newNode;
+}
+
+void insertAtEnd(struct Node **head, int value)
+{
+  struct Node *newNode = createNewNode(value);
+  if (*head == NULL)
+  {
+    *head = newNode;
+    return;
+  }
+  struct Node *temp = *head;
+  while (temp->next != NULL)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+}
+
+void displayList(struct Node *head)
+{
+  struct Node *temp = head;
+  while (temp->next != NULL)
+  {
+    printf("%d -> ", temp->data);
+    temp = temp->next;
+  }
+  printf("NULL");
+}
+
+void deleteNode(struct Node **head, int key)
+{
+  struct Node *temp = *head;
+  struct Node *prev = NULL;
+
+  if (temp != NULL && temp->data == key)
+  {
+    *head = temp->next;
+    free(temp);
+    return;
+  }
+
+  while (temp->data != key && temp != NULL)
+  {
+    prev = temp;
+    temp = temp->next;
+  }
+
+  if (temp == NULL)
+    return;
+}
+
+int main()
+{
+  return 0;
+}
