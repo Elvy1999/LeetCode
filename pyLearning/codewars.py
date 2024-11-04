09d778a3220bf2d51b27ef59c3366371a74b3e6c
Elvy1999 <elvydecoo99@gmail.com>
2024-11-04 17:10:54 -0500

    completeded a kata

--- pyLearning/codewars.py
+++ pyLearning/codewars.py
@@ -7,3 +7,17 @@ def get_count(sentence):
   return(count)
 
 print(get_count("aeiou"))
+
+def beeramid(bonus, price):
+    if(bonus <= 0): return 0
+    totalBeers = bonus/price
+    summedBeers = 0
+    i = 0
+    while(summedBeers <= totalBeers):
+      summedBeers += i**2
+      i+=1
+    if(summedBeers > totalBeers): 
+      i -= 2
+    return i;
+  
+print(beeramid(5000, 3))
