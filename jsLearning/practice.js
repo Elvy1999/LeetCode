85118906a1d09a0c7ee50db27c73c6e92f68533b
Elvy1999 <elvydecoo99@gmail.com>
2024-11-07 19:28:41 -0500

    did fibonacci

--- jsLearning/practice.js
+++ jsLearning/practice.js
@@ -27,6 +27,32 @@ sumAll = (num1, num2) => {
 
 const leapYears = (year) => year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
 
+palindromes = (word) => {
+  let begin = 0;
+  let end = word.length - 1;
+  while (begin < end) {
+    if (word[begin] != word[end]) return false;
+    begin++;
+    end--;
+  }
+  return true;
+};
+
+fibonnaci = (num) => {
+  let a = 0;
+  let b = 1;
+  fibs = [];
+  for (let i = 0; i < num; i++) {
+    fibs.push(a);
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return fibs;
+};
+
+console.log(fibonnaci(10));
+
 console.log(reverseString("Hello"));
 console.log(removeFromArray([1, 2, 3, 4], 3));
 console.log(sumAll(1, 4));
